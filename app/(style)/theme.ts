c5fe9b24a222380ac066512835e0a2d54dde3b7b
maxencetasse <maxence.tasse@gmail.com>
2025-07-18 00:23:56 +0200

    Add Material UI in the test project

---
+++ app/(style)/theme.ts
@@ -0,0 +1,5 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  // Personnalise ton thème ici si besoin
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./(style)/theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
